d12e7faf4eaf76e3af9483f3a95de4620bf64df1
verosardin <vero.sardin28@gmail.com>
2020-03-23 13:07:25 -0300

    cambio en hola.js

--- hola.js
+++ hola.js
@@ -1,5 +1,5 @@
-let nombre1= "vero";
+let nombre1= "vero Sardin";
 let nombre2="marie";
 let nombre3="tincho";
 
-console.log(`Hola ${nombre1} , ${nombre2} , ${nombre3}`)
+console.log(`Hola ${nombre1} , ${nombre2} , ${nombre3}, buenas tardes`)
