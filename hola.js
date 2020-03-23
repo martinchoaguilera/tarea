0237b208914106ff2ae3da3399e5b0836ce6be3e
martinchoaguilera <martinale93aguilera@gmail.com>
2020-03-23 13:03:49 -0300

    modifique el archivo hola.js

--- hola.js
+++ hola.js
@@ -1,5 +1,12 @@
+
+
+
 let nombre1= "vero";
 let nombre2="marie";
 let nombre3="tincho";
 
 console.log(`Hola ${nombre1} , ${nombre2} , ${nombre3}`)
+
+let edad1 = 26;
+
+console.log(`${nombre3} tiene ${edad1} años`) ;
